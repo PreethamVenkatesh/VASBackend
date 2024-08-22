0b2572ed5d7a49a066814092ca9e3e7e54350b04
Preetham Venkatesh <preetham.venkyy@gmail.com>
2024-08-22 02:48:04 +0100

    Customer Login and Signup

--- models/Customer.js
+++ models/Customer.js
@@ -1,34 +1,36 @@
-const mongoose = require('mongoose'); // Import the mongoose library
+const bcrypt = require('bcryptjs'); 
+const mongoose = require('mongoose');
 
+// Define the schema for the Customer collection
 const custSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    // required: true
+    required: true
   },
   lastName: {
     type: String,
-    // required: true
+    required: true
   },
   emailId: {
     type: String,
-    // required: true,
-    // unique: true
+    required: true,
+    unique: true
   },
   phoneNumber: {
-    type: String, // Changed from Number to String to handle leading zeros
-    // required: true,
-    // unique: true
+    type: String, 
+    required: true,
+    unique: true
   },
   password: {
     type: String,
-    // required: true
+    required: true
   },
-  // createdAt: {
-    // type: Date,
-    // // default: Date.now
-  // }
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 const Customer = mongoose.model('Customer', custSchema)
 
 module.exports = Customer;
-// module.exports = mongoose.model('Customer', custSchema);
+
